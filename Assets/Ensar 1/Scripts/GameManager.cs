e05ba7ec2caac6a4a7ee36bc6a0cc280a43a1ee7
ensarzcn <ensarzcn58@gmail.com>
2025-06-15 20:33:51 +0300

    P1 / P2 SELECT

--- Assets/Ensar 1/Scripts/GameManager.cs
+++ Assets/Ensar 1/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     public GameObject player1Prefab;
     public GameObject player2Prefab;
 
-    public GameObject[] allCharacters; // 4 karakter prefab'�
+    public GameObject[] allCharacters; // 4 karakter prefab'ı
     public int player1Index = -1;
     public int player2Index = -1;
 
@@ -46,13 +46,13 @@ public class GameManager : MonoBehaviour
     {
         if (scene.name == "Level2")
         {
-            // Se�ilen index'lere g�re prefab'lar� belirle
+            // Seçilen index'lere göre prefab'ları belirle
             if (player1Index >= 0 && player1Index < allCharacters.Length)
                 player1Prefab = allCharacters[player1Index];
             if (player2Index >= 0 && player2Index < allCharacters.Length)
                 player2Prefab = allCharacters[player2Index];
 
-            // Spawn noktalar�n� bul
+            // Spawn noktalarını bul
             if (player1SpawnPoint == null)
                 player1SpawnPoint = GameObject.Find("Player1SpawnPoint")?.transform;
             if (player2SpawnPoint == null)
@@ -64,18 +64,69 @@ public class GameManager : MonoBehaviour
             if (player2Prefab != null && player2SpawnPoint != null)
                 player2Instance = Instantiate(player2Prefab, player2SpawnPoint.position, Quaternion.identity);
 
-            // Kamera hedeflerini ayarla
-            MultiTargetCamera multiCam = Camera.main.GetComponent<MultiTargetCamera>();
-            if (multiCam != null)
+            // PLAYER 1 CANVAS AYARI
+            if (player1Instance != null)
             {
-                List<Transform> camTargets = new List<Transform> {
-                    player1Instance.transform,
-                    player2Instance.transform
-                };
-                multiCam.SetTargets(camTargets);
+                Transform canvas1 = player1Instance.transform.Find("Canvas1");
+                Transform canvas2 = player1Instance.transform.Find("Canvas2");
+
+                if (canvas1 != null) canvas1.gameObject.SetActive(true);
+                if (canvas2 != null) canvas2.gameObject.SetActive(false);
+            }
+
+            // PLAYER 2 CANVAS AYARI
+            if (player2Instance != null)
+            {
+                Transform canvas1 = player2Instance.transform.Find("Canvas1");
+                Transform canvas2 = player2Instance.transform.Find("Canvas2");
+
+                if (canvas1 != null) canvas1.gameObject.SetActive(false);
+                if (canvas2 != null) canvas2.gameObject.SetActive(true);
+            }
+
+
+            // ✨ PLAYER 1: Movement & Shooting ✨
+            if (player1Instance != null)
+            {
+                var p1Move = player1Instance.GetComponent<Player1Movement>();
+                var p2Move = player1Instance.GetComponent<Player2Movement>();
+                var p1Shoot = player1Instance.GetComponent<Player1Shooting>();
+                var p2Shoot = player1Instance.GetComponent<Player2Shooting>();
+
+                if (p1Move != null) p1Move.enabled = true;
+                if (p2Move != null) p2Move.enabled = false;
+
+                if (p1Shoot != null) p1Shoot.enabled = true;
+                if (p2Shoot != null) p2Shoot.enabled = false;
+            }
+
+            // ✨ PLAYER 2: Movement & Shooting ✨
+            if (player2Instance != null)
+            {
+                var p1Move = player2Instance.GetComponent<Player1Movement>();
+                var p2Move = player2Instance.GetComponent<Player2Movement>();
+                var p1Shoot = player2Instance.GetComponent<Player1Shooting>();
+                var p2Shoot = player2Instance.GetComponent<Player2Shooting>();
+
+                if (p1Move != null) p1Move.enabled = false;
+                if (p2Move != null) p2Move.enabled = true;
+
+                if (p1Shoot != null) p1Shoot.enabled = false;
+                if (p2Shoot != null) p2Shoot.enabled = true;
+            }
+
+            // Kamera hedeflerini ayarla
+            MultiTargetCamera cam = Camera.main.GetComponent<MultiTargetCamera>();
+            if (cam != null)
+            {
+                cam.targets.Clear(); // önceki hedefleri temizle
+                cam.targets.Add(player1Instance.transform);
+                cam.targets.Add(player2Instance.transform);
             }
         }
     }
+
+
 }
-    
+
 
